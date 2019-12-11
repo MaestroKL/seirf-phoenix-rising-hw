477c5d53a6a5d19837825e5c0e512e7f5f02eae3
Kevin Lai <maestrokl@Kevins-Mac-mini.fios-router.home>
2019-12-11 12:04:45 -0500

    finished loop practice

---
+++ first_homework/loops.js
@@ -0,0 +1,34 @@
+// Create a loop that logs number from 0-99 (ascendiing)
+for (i = 0; i<= 99; i++) {
+	console.log(i)
+}
+
+// Create a loop that logs the EVEN number from 0-99 (ascending)
+for (i = 0; i<= 99; i++) {
+	if (i%2===0) {
+	console.log(i);
+	}
+	else {}
+}
+
+// Create a loop that logs the ODD numbers from 0-99 (ascending)
+for (i = 0; i<= 99; i++) {
+	if (i%2===1) {
+	console.log(i);
+	}
+	else {}
+}
+
+// Create a loop that logs the numbers from 49-72 (ascending)
+for (i = 49; i<= 72; i++) {
+	console.log(i)
+}
+
+// Create a loop that logs the numbers from 3-90 that are multiples of 3 (ascending)
+for (i = 3; i<= 90; i++) {
+	if (i%3===0) {
+	console.log(i);
+	}
+	else {}
+}
+
