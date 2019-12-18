6e83c3082e67f876963dfbf9531d1cb280fcc408
Kevin Lai <maestrokl@Kevins-Mac-mini.fios-router.home>
2019-12-18 05:22:02 -0500

    Wild Wild Life answered

--- second_homework/answer.js
+++ second_homework/answer.js
@@ -9,16 +9,23 @@
 //     console.log (i)}
 // }
 
-// Fizz buzz
-for (i=0; i <=100; i++){
-    if (i%5===0 && i%3===0) {
-        console.log ("FizzBuzz")
-    }
-    else if (i%5===0) {
-        console.log ("Buzz")
-    }
-    else if (i%3===0) {
-        console.log ("Fizz")
-    } 
-    else {console.log (i)}
-}
+// // Fizz buzz
+// for (i=0; i <=100; i++){
+//     if (i%5===0 && i%3===0) {
+//         console.log ("FizzBuzz")
+//     }
+//     else if (i%5===0) {
+//         console.log ("Buzz")
+//     }
+//     else if (i%3===0) {
+//         console.log ("Fizz")
+//     } 
+//     else {console.log (i)}
+// }
+
+// Wild Wild Life
+1. plantee [2] = 197
+2. wolfy [3] = "Gotham City"
+3. dart.push = "Hawkins"
+4. wolfy [1] = "Gameboy"
+
