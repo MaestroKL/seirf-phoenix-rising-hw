835897beccb5069b8b77d5335956594fd4c49a87
Kevin Lai <maestrokl@Kevins-Mac-mini.fios-router.home>
2019-12-18 05:17:32 -0500

    Fizz Buzz answered

--- second_homework/answer.js
+++ second_homework/answer.js
@@ -1,10 +1,24 @@
-// Easy Going: 
+// // Easy Going: 
 // for (i=0; i <=20; i++){
 //     console.log (i)
 // }
 
-// Get Even
-for (i=0; i <=200; i++){
-    if (i%2===0) {
-    console.log (i)}
+// // Get Even
+// for (i=0; i <=200; i++){
+//     if (i%2===0) {
+//     console.log (i)}
+// }
+
+// Fizz buzz
+for (i=0; i <=100; i++){
+    if (i%5===0 && i%3===0) {
+        console.log ("FizzBuzz")
+    }
+    else if (i%5===0) {
+        console.log ("Buzz")
+    }
+    else if (i%3===0) {
+        console.log ("Fizz")
+    } 
+    else {console.log (i)}
 }
