d07d8d737ae63257e520520302c7e81aaeda9196
Kevin Lai <maestrokl@Kevins-Mac-mini.fios-router.home>
2019-12-18 04:08:53 -0500

    palindrome completed

--- second_homework/solutions.js
+++ second_homework/solutions.js
@@ -2,3 +2,18 @@
 // -A parameter is the variable in a declared function.  An argument is an actual value of the parameter that is pushed through a function. 
 // Within a function,w hat is the difference between return and console.log?
 
+// Palindrome = Did not get. I looked at the solutions. Why is .join even necessary?
+
+// let wordForward = "Five blind mice" 
+// let lowerWord = wordForward.toLowerCase ();
+// let splitWord = lowerWord.split ("");
+// let reverseWord = splitWord.reverse();
+// if (splitWord == reverseWord){
+//     return true
+// }
+// else {
+//     return false
+// }
+// console.log (wordForward('Hello World'))
+
+
