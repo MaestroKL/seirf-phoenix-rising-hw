f9d00db457dd4a925da0aa1ee95d9a0cd2d1dd4f
Kevin Lai <maestrokl@Kevins-Mac-mini.local>
2019-12-28 15:59:39 -0500

    crazy object complete

--- third_homework/answer.js
+++ third_homework/answer.js
@@ -31,9 +31,43 @@
 //     ]
 
     // TAKE IT EASY
-let rainbow = ["blue"]
-console.log (rainbow[0])
-let kevin = {favoriteFood: "chicken", hobby: "tennis", location: "boston", favoriteDatatype: "strings"}
-console.log (kevin.hobby)
+// let rainbow = ["blue"]
+// console.log (rainbow[0])
+// let kevin = {favoriteFood: "chicken", hobby: "tennis", location: "boston", favoriteDatatype: "strings"}
+// console.log (kevin.hobby)
 
+// CRAZY OBJECTS
+const crazyObject = {
+    taco: [{meat: 'steak',
+           cheese: ['panela', 'queso', 'chihuahua']},
+           {meat: 'chicken',
+            salsa: ["pico", "hot", "hotter", "really hot", "really really hot", "omg my mouth is burning"]},
+          ],
+    larry: {
+      nicknames: ["LD", "Chicken Teriyaki Boyyyyyy"],
+      quotes: ["Pretty pretty prettayyyyy good", "Is that a parkinson's thing?", "women love a self confident bald man", "I'm a walking talking enigma"],
+      characters: [{
+        name: "Jeff",
+        occupation: "manager"
+      },
+      {
+        name: "funkhauser",
+        occupation: "tv dude"
+      },
+      {
+        name: "susie",
+        occupation: "jeffs wife",
+        favourtieHobby: "Swearing at Larry and Jeff"
+      },
+      ]
+    }
+  }
 
+  console.log (crazyObject.taco[1].salsa[5])
+  console.log (crazyObject.larry.quotes[0])
+  console.log (crazyObject.larry.characters[2].favourtieHobby)
+  console.log (crazyObject.larry.nicknames[1])
+  console.log (crazyObject.larry.characters[1])
+  crazyObject.larry.quotes.push ("I'm tryin to elevate small talk to medium talk")
+  console.log (crazyObject.larry.quotes)
+  
