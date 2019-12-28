24122ba4be331d5db3302f950e9fba1fe76bc388
Kevin Lai <maestrokl@Kevins-Mac-mini.local>
2019-12-28 15:27:24 -0500

    datatype refresher complete

---
+++ third_homework/answer.js
@@ -0,0 +1,31 @@
+A light switch that can be either on or off: 
+    -Datatype: Boolean
+    -Data structure: let lightON = true
+A user's email address: 
+    -Datatype: Object
+    -Data structure: let emailList = {person: "John", email: "doe@gmail.com" }
+A spaceship with a hull, laser blasters, tractor beam and warp drive: 
+    -Datatype: Array
+    -Data structure: let spaceshipParts = ["hull", "laser blasters", "tractor beam", "warp drive"] 
+A list of student names from our class: 
+    -Datatype: Array
+    -Data structure: let classList = ["Peter", "Gertrude", "Mary" , "John"]
+A list of student names from our class, each with a location: 
+    -Datatype: Array of Objects
+    -Data structure: classmateLocation = [
+        {Name: "John", 
+        Location: "1st floor"}, 
+        {Name: "Mark", 
+        Location: "2nd floor"}
+    ]
+A list of student names from our class, each with a location and each with a list of favorite TV shows: 
+    -Datatype: Array of objects
+    -Data structure: 
+        Data structure: classmateLocation = [
+        {Name: "John", 
+        Location: "1st floor", 
+        FavoriteTv: "Friends"}, 
+        {Name: "Mark", 
+        Location: "2nd floor", 
+        FavoriteTv: "Seinfeld"}
+    ]
