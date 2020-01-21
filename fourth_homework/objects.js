606f565a6d52aab362ba1e7c070584aac9f7b11c
Kevin Lai <maestrokl@Kevins-Mac-mini.fios-router.home>
2020-01-21 09:29:02 -0500

    created hamster class

---
+++ fourth_homework/objects.js
@@ -0,0 +1,19 @@
+class Hamster {
+    owner: "", 
+    constructor(name){
+        this.name = 
+    }
+    price: 15
+    wheelRun() {
+        console.log("squeak squeak")
+    }
+    eatFood() {
+        console.log("nibble nibble")
+    }
+    getPrice(){
+        return price
+    }
+
+
+}
+console.log
