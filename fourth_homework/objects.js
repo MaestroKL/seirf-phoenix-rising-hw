7ad0fb03a7f6ad25b20408517cdbceb82b8b0e15
Kevin Lai <maestrokl@Kevins-Mac-mini.fios-router.home>
2020-01-21 09:42:05 -0500

    created person class

--- fourth_homework/objects.js
+++ fourth_homework/objects.js
@@ -13,7 +13,49 @@ class Hamster {
     getPrice(){
         return price
     }
-
-
 }
-console.log
+
+class Person {
+    constructor(name){
+        this.name = name;
+        this.age = 0;
+        this.height = 0;
+        this.weight = 0;
+        this.mmod = 0;
+        this.hamster = [
+        ];
+        this.bankAccount = 0;
+    getName(){
+        return this.name
+    }
+    getAge(){
+        return this.name
+    }
+    getWeight(){
+        return this.weight
+    }
+    greet(){
+        console.log("Hello" + this.name)
+    }
+    eat(){
+        this.weight= this.weight + 1
+        this.mood= this.mood + 1
+    }
+    exercise(){
+        this.weight=this.weight-1
+    }
+    ageUp(){
+        this.age=this.age + 1
+        this.height=this.height + 1
+        this.weight = this.weight + 1
+        this.mood = this.mood - 1
+        this.bankAccount = this.bankAccount + 10
+    }
+
+    buyHamster(hamster){
+        this.buyHamster.push (hamster)
+        this.mood = this.mood - 10
+        this.bankAccount=this.bankAccount - Hamster.getPrice
+    }
+    }
+}
