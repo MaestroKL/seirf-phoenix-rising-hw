2b69832cf6c5f5afcb6a2b6283f74b57d5bdd3d2
Kevin Lai <maestrokl@Kevins-Mac-mini.fios-router.home>
2020-01-21 11:38:27 -0500

    created our enemy rate

--- fourth_homework/donut_adventure/adventure.js
+++ fourth_homework/donut_adventure/adventure.js
@@ -1,28 +1,51 @@
 class Hero {
-    constructor (name){
+    constructor (name, health, weapons, catchPhrases){
         this.name = name;
         this.health = 100;
         this.weapons = [
-            {name: "sprinkleSpray"
-            hp: 5}
-            {name: "sugarShock"
-            hp: 10}
+            {
+                sprinkleSpray: 5,
+                sugarShock: 10
+            }
         ]
         this.catchPhrases = ['i\'m fresher than day old pizza', 
         'you can\'t count my calories']
-        talkSass() {
-            console.log ("randomCatchphrase")
-        }
-        announceHealth(){
-            console.log(this.health)
-        }
-        fight(){
-            console.log("I\'m ready to rumble")
-        }
+    }
 
+    talkSass(){
+        console.log(Math.floor(Math.random()*this.catchPhrases.length))
+    }
+    announceHealth(){
+        console.log(this.name + "'s health is " + this.health)
+    }
+    fight(){
+        console.log("I\'m ready to rumble")
+    }
+    }
+
+class Enemy {
+    constructor(name, health, weapons, catchPhrases){
+        this.name = name;
+        this.health = 100;
+        this.weapons = [
+            {
+                pepperoniStars: 5,
+                cheeseGrease: 10    
+            }
+        ]
+        this.catchPhrases = 
+        ['i\'m youtube famous','i\'m more dangerous than an uncovered sewer']
+    }
+    talkSmack() {
+        console.log(Math.floor(Math.random()*this.catchPhrases.length))
+    }
+    announceHealth(){
+        console.log(this.name + "'s health is " + this.health)
+    }
+    fight() {
+        console.log("I\'m gonna flatten you like a slice of pepperoni!")
     }
 }
 
-class Enemy {
-
-}
+let PizzaRat = new Enemy (this.name = "Pizza Rat")
+console.log(PizzaRat)
