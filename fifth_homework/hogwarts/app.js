21334a96cde850c29ff938c9910e553dd98a8818
Kevin Lai <maestrokl@Kevins-Mac-mini.fios-router.home>
2020-01-29 18:53:36 -0500

    added my first element and it was fun

--- fifth_homework/hogwarts/app.js
+++ fifth_homework/hogwarts/app.js
@@ -1,5 +1,22 @@
-if (typeof jQuery=='undefined'){
-        console.log("'oops! I still have to link my jQuery properly");
-}else{
-    console.log("I did it! I linked jQuery and this js file!")
-}
+// if (typeof jQuery=='undefined'){
+//         console.log("'oops! I still have to link my jQuery properly");
+// }else{
+//     console.log("I did it! I linked jQuery and this js file!")
+// }
+
+$(()=>{
+
+$container = $("#container")
+console.log($container)
+$h1 = $("<h1>")
+$('body').append($h1)
+console.log($h1)
+$h1.text("Hogwarts")
+
+
+
+
+
+
+
+})
